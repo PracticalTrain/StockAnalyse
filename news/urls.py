99c178778c75a6965f94c711a59522ad8c49d090
jjzhu <jjzhu_ncu@163.com>
2016-12-19 19:18:13 +0800

    新闻页面分页处理

--- news/urls.py
+++ news/urls.py
@@ -3,8 +3,7 @@ from django.conf.urls import url
 from . import views
 app_name = 'news'
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
-    url(r'^china_map', views.china_map, name='china_map'),
-    url(r'^map', views.map, name='map'),
-    url(r'(?P<news_id>[0-9]+)/$', views.detail, name='detail'),
+
+    url(r'^p/(?P<page_num>[0-9]+)/$', views.get_news_by_pages, name='page'),
+    url(r'^d/(?P<news_id>[0-9]+)/$', views.detail, name='detail'),
 ]
